eaa437b0960d5b92831741d7826cd94904969ccd
Maliy Igor <imaliy.filsh@gmail.com>
2015-07-06 18:41:45 +0300

    User social authorize

--- console/runners/identity/Create.php
+++ console/runners/identity/Create.php
@@ -2,7 +2,9 @@
 
 namespace console\runners\identity;
 
+use dektrium\user\Finder;
 use dektrium\user\models\User;
+use dektrium\user\models\Account;
 use filsh\wamp\exceptions\ErrorException;
 use filsh\wamp\exceptions\ModelValidationException;
 
@@ -14,20 +16,38 @@ class Create extends \console\runners\Identity
     
     public $password;
     
+    public $accountId;
+    
     protected function doRun()
+    {
+        $account = null;
+        if($this->accountId !== null) {
+            $finder = \Yii::$container->get(Finder::className());
+            $account = $finder->findAccountById($this->accountId);
+        }
+        
+        $user = $this->create($account);
+        
+        if ($user->hasErrors()) {
+            return $this->formatModelError($user);
+        }
+        return $this->formatIdentityResult($user);
+    }
+    
+    protected function create(Account $account = null)
     {
         /* @var $user \dektrium\user\models\User */
         $user = \Yii::createObject([
             'class'    => User::className(),
-            'scenario' => 'create',
+            'scenario' => $account === null ? 'create' : 'connect',
             'email' => $this->email,
             'username' => $this->username,
             'password' => $this->password,
         ]);
-
-        if ($user->create()) {
-            return $this->formatIdentityResult($user);
+        
+        if($user->create() && $account !== null) {
+            $account->link('user', $user);
         }
-        return $this->formatModelError($user);
+        return $user;
     }
 }
